550c78a848ba3afdfbb94a8d10062ae3328c1e00
Christian Zenker <christian.zenker@599media.de>
2012-04-13 09:33:32 +0200

    template directory determination

--- Classes/View/TwigView.php
+++ Classes/View/TwigView.php
@@ -2,13 +2,33 @@
 
 class Tx_ExtbaseTwig_View_TwigView implements Tx_Extbase_MVC_View_ViewInterface {
 
+    /**
+     * @var bool
+     */
+    protected static $twigAutoloaderInitialized = false;
+
 
     /**
      * @var Tx_Extbase_MVC_Controller_ControllerContext
      */
     protected $controllerContext;
 
+    /**
+     * variables for the template
+     * 
+     * @var array
+     */
     protected $variables = array();
+
+    /**
+     * @var Twig_Loader_Filesystem
+     */
+    protected $loader;
+
+    /**
+     * @var string
+     */
+    protected $templateRootPathPattern = '@packageResourcesPath/Private/Templates';
 
     /**
      * Sets the current controller context
@@ -74,14 +94,18 @@ class Tx_ExtbaseTwig_View_TwigView implements Tx_Extbase_MVC_View_ViewInterface 
      */
     public function render()
     {
-        $this->initTwigAutoloader();
 
-        $loader = new Twig_Loader_Filesystem('typo3conf/ext/twig_hello/Resources/Templates/Default');
-        $twig = new Twig_Environment($loader, array(
+        $controllerName = $this->controllerContext->getRequest()->getControllerName();
+        $actionName = $this->controllerContext->getRequest()->getControllerActionName();
+        $formatName = $this->controllerContext->getRequest()->getFormat();
+
+        $templateName = $controllerName.'/'.$actionName.'.'.$formatName.'.twig';
+
+        $twig = new Twig_Environment($this->loader, array(
             'cache' => 'typo3temp/twig/',
         ));
 
-        return $twig->render('index.html.twig', $this->variables);
+        return $twig->render($templateName, $this->variables);
     }
 
     /**
@@ -91,10 +115,34 @@ class Tx_ExtbaseTwig_View_TwigView implements Tx_Extbase_MVC_View_ViewInterface 
      * @api
      */
     public function initializeView() {
+        $this->initTwigAutoloader();
+
+        $extKey = $this->controllerContext->getRequest()->getControllerExtensionKey();
+        $defaultTemplateRootPath = t3lib_extMgm::extPath($extKey).'Resources/Private/Templates';
+
+        $this->loader = new Twig_Loader_Filesystem(array($defaultTemplateRootPath));
+
     }
 
+    /**
+     * make sure twig autoloader is initialized
+     */
     protected function initTwigAutoloader() {
-        require_once t3lib_extMgm::extPath('extbase_twig').'Vendor/Twig/lib/Twig/Autoloader.php';
-        Twig_Autoloader::register();
+        if(!self::$twigAutoloaderInitialized) {
+            require_once t3lib_extMgm::extPath('extbase_twig').'Vendor/Twig/lib/Twig/Autoloader.php';
+            Twig_Autoloader::register();
+            self::$twigAutoloaderInitialized = true;
+        }
+    }
+
+    /**
+     * add another template root path
+     *
+     * @param $templateRootPath
+     */
+    public function setTemplateRootPath($templateRootPath) {
+        // @todo this could be an array with fallback
+
+        $this->loader->addPath($templateRootPath);
     }
 }
