77a3e4bbcae6875e51d66653fad0109687111a42
Christian Zenker <christian.zenker@599media.de>
2012-04-13 08:09:36 +0200

    initial setup with Twig as submodule

---
+++ ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "extbase_twig".
+#
+# Auto generated 03-04-2012 22:08
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Twig for Extbase',
+	'description' => '',
+	'category' => '',
+	'author' => 'Christian Zenker',
+	'author_email' => 'christian.zenker@599media.de',
+	'author_company' => '599media GmbH',
+	'shy' => '',
+	'dependencies' => 'cms,extbase',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'experimental',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'cms' => '',
+			'extbase' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => 'a:2:{s:17:"ext_localconf.php";s:4:"c8ba";s:14:"ext_tables.php";s:4:"2836";}',
+);
+
+?>
