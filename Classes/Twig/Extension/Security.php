f04925e18a33769c41537b26e0d823506c7a8407
Christian Zenker <christian.zenker@599media.de>
2012-04-15 11:07:51 +0200

    has_role function

--- Classes/Twig/Extension/Security.php
+++ Classes/Twig/Extension/Security.php
@@ -7,12 +7,25 @@ class Tx_ExtbaseTwig_Twig_Extension_Security extends Twig_Extension
 	{
 		return array(
 			'is_authenticated' => new Twig_Function_Function('Tx_ExtbaseTwig_Twig_Extension_Security::is_authenticated'),
+			'has_role' => new Twig_Function_Function('Tx_ExtbaseTwig_Twig_Extension_Security::has_role'),
 		);
 	}
 
 	public static function is_authenticated()
 	{
 		return isset($GLOBALS['TSFE']) && $GLOBALS['TSFE']->loginUser;
+	}
+
+	public static function has_role($role)
+	{
+		if (!isset($GLOBALS['TSFE']) || !$GLOBALS['TSFE']->loginUser) {
+			return FALSE;
+		}
+		if (is_numeric($role)) {
+			return (is_array($GLOBALS['TSFE']->fe_user->groupData['uid']) && in_array($role, $GLOBALS['TSFE']->fe_user->groupData['uid']));
+		} else {
+			return (is_array($GLOBALS['TSFE']->fe_user->groupData['title']) && in_array($role, $GLOBALS['TSFE']->fe_user->groupData['title']));
+		}
 	}
 
 
